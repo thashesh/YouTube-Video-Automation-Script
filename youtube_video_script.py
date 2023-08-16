c01b8144d42f2445adca0c6d5102f136ea240b02
thashesh <102394347+thashesh@users.noreply.github.com>
2023-08-16 15:39:40 -0700

    opens multiple videos in different tabs

--- youtube_video_script.py
+++ youtube_video_script.py
@@ -1,59 +1,50 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 import time
 
 # Define constants
-CSV_FILES = ["file1.csv", "file2.csv"]  # Replace with your file names
+CSV_FILES = ["csv1.csv"]  # Replace with your file names
 CHROME_DRIVER_PATH = r"C:\Users\ashes\PycharmProjects\async_url_automation\chromedriver.exe"  # Replace with your ChromeDriver path
-URL_COLUMN = "url"  # Replace with the column name containing the URLs in your CSV files
 
 # Initialize the web driver
+from selenium.webdriver.chrome.service import Service
+service = Service(executable_path=CHROME_DRIVER_PATH)
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-notifications")
 options.add_argument("--start-maximized")
 options.add_argument("--mute-audio")
-driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH, chrome_options=options)
-
+driver = webdriver.Chrome(service=service, options=options)
 
 # Read CSV files and extract URLs
 urls = []
 for csv_file in CSV_FILES:
-    df = pd.read_csv(csv_file)
-    urls += df[URL_COLUMN].tolist()
+    df = pd.read_csv(csv_file, header=None)
+    urls += df.iloc[0].tolist()
 
-# Define a function to wait until the video is finished
-def wait_video_completion():
-    try:
-        WebDriverWait(driver, 3600).until(
-            EC.invisibility_of_element_located((By.CLASS_NAME, "playing-mode"))
-        )
-    except Exception as e:
-        print(f"Error waiting for video completion: {e}")
-
-# Open each URL, play the video, and close the tab upon completion
+# Open each URL in a new tab
 for url in urls:
+    driver.execute_script(f"window.open('{url}', '_blank');")
+
+# Switch to each tab and play the video
+for i in range(1, len(urls) + 1):
     try:
-        driver.execute_script(f"window.open('{url}', '_blank');")
-        driver.switch_to.window(driver.window_handles[-1])
+        driver.switch_to.window(driver.window_handles[i])
+        time.sleep(2) # Give the page some time to load
 
-        # Wait for video to start playing
-        WebDriverWait(driver, 15).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, "playing-mode"))
+        # Click the play button (For YouTube, the class name of the play button is 'ytp-large-play-button')
+        play_button = WebDriverWait(driver, 15).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "ytp-large-play-button"))
         )
-
-        # Wait for video to finish
-        wait_video_completion()
-
-        # Close the tab and switch back to the main window
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
+        play_button.click()
 
     except Exception as e:
-        print(f"Error processing URL {url}: {e}")
+        print(f"Error processing URL {urls[i-1]}: {e}")
 
-# Close the browser
-driver.quit()
+# Switch back to the main window if needed
+driver.switch_to.window(driver.window_handles[0])
+
+# You can close the browser after all the actions are done
+# driver.quit()
